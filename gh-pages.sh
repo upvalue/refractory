8e7d3c0c3e36a9dba5fbd14e2ea54ec0887a88b8
Phil <phil@upvalue.io>
2019-11-24 22:30:53 -0700

    Update script

--- gh-pages.sh
+++ gh-pages.sh
@@ -3,13 +3,13 @@
 
 set -x
 
+yarn build
 cd ..
-mkdir -p rf-gh-pages
+rm -rf rf-gh-pages
+mkdir rf-gh-pages
 git clone ./refractory rf-gh-pages
 cd rf-gh-pages
-yarn build
 git checkout gh-pages
-cp -ru build/* .
+cp -ru ../refractory/build/* .
+git add .
 git commit -a
-
-
