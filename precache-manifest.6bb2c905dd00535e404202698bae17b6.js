55f626f428401e84756974ff1218dc206525ddb3
Phil <phil@upvalue.io>
2019-11-24 22:31:40 -0700

    Build

---
+++ precache-manifest.6bb2c905dd00535e404202698bae17b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b717df16c733011b54f0933e45f725",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0abf836b2bf8e72a6b09",
+    "url": "/static/css/main.e5cf6bdf.chunk.css"
+  },
+  {
+    "revision": "cade82aede2b7279e83e",
+    "url": "/static/js/2.c8341eca.chunk.js"
+  },
+  {
+    "revision": "0abf836b2bf8e72a6b09",
+    "url": "/static/js/main.5665da6e.chunk.js"
+  },
+  {
+    "revision": "1284ce65b732e51f19f1",
+    "url": "/static/js/runtime-main.47a5fbbf.js"
+  }
+]);
