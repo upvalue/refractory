813f10f7424d31aaba2424652dcf14c23db2ce25
Phil <phil@upvalue.io>
2019-11-24 22:38:31 -0700

    Thing

---
+++ precache-manifest.0d7f1499109a2a845d2feb99bcafd4d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdcd54d716fb800336fcc6576c796bef",
+    "url": "/refractory/index.html"
+  },
+  {
+    "revision": "82cf8e41bc4acdcc809a",
+    "url": "/refractory/static/css/main.e5cf6bdf.chunk.css"
+  },
+  {
+    "revision": "cade82aede2b7279e83e",
+    "url": "/refractory/static/js/2.c8341eca.chunk.js"
+  },
+  {
+    "revision": "82cf8e41bc4acdcc809a",
+    "url": "/refractory/static/js/main.f112904b.chunk.js"
+  },
+  {
+    "revision": "331bdc94612daaab64b3",
+    "url": "/refractory/static/js/runtime-main.10d9f64a.js"
+  }
+]);
